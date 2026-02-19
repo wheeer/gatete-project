067d85e47be6b1feca81e8d3db33eefc49f810a8
Walter <whollub@gmail.com>
2026-02-19 00:50:27 -0300

    estados de ataque

--- Player/don_gatoCombat.gd
+++ Player/don_gatoCombat.gd
@@ -5,9 +5,21 @@ signal attack_started
 signal attack_finished
 
 @export var attack_damage: float = 25.0
-@export var attack_duration: float = 0.10
+@export var startup_time: float = 0.08
+@export var active_time: float = 0.05
+@export var recovery_time: float = 0.15
+
 @export var attack_cooldown: float = 0.28
 @export var attack_stamina_cost: float = 15.0
+
+
+enum AttackPhase {
+	NONE,
+	STARTUP,
+	ACTIVE,
+	RECOVERY
+}
+var current_phase: AttackPhase = AttackPhase.NONE
 
 var stats: DonGatoStats
 
@@ -18,6 +30,7 @@ var is_attacking: bool = false
 var cooldown_timer: float = 0.0
 var attack_timer: float = 0.0
 var already_hit: bool = false
+
 
 func _ready() -> void:
 	pass
@@ -31,13 +44,27 @@ func setup(_body: CharacterBody3D, _attack_area: Area3D, _stats: DonGatoStats) -
 	attack_area.area_entered.connect(_on_attack_area_area_entered)
 	
 func _physics_process(delta: float) -> void:
+	
 	if cooldown_timer > 0:
 		cooldown_timer -= delta
 	
 	if is_attacking:
 		attack_timer -= delta
+		
 		if attack_timer <= 0:
-			_end_attack()
+			match current_phase:
+				AttackPhase.STARTUP:
+					current_phase = AttackPhase.ACTIVE
+					attack_timer = active_time
+					attack_area.monitoring = true
+				
+				AttackPhase.ACTIVE:
+					current_phase = AttackPhase.RECOVERY
+					attack_timer = recovery_time
+					attack_area.monitoring = false
+				
+				AttackPhase.RECOVERY:
+					_end_attack()
 
 func try_attack() -> void:
 	if is_attacking:
@@ -53,14 +80,15 @@ func try_attack() -> void:
 
 func _start_attack() -> void:
 	is_attacking = true
-	attack_timer = attack_duration
+	current_phase = AttackPhase.STARTUP
+	attack_timer = startup_time
 	cooldown_timer = attack_cooldown
 	already_hit = false
 	
-	attack_area.monitoring = true
 	emit_signal("attack_started")
 
 func _on_attack_area_area_entered(area: Area3D) -> void:
+	print("Golpe en fase:", current_phase)
 	var enemy = area.get_parent()
 	
 	if enemy.has_method("take_damage"):
@@ -68,5 +96,6 @@ func _on_attack_area_area_entered(area: Area3D) -> void:
 
 func _end_attack() -> void:
 	is_attacking = false
+	current_phase = AttackPhase.NONE
 	attack_area.monitoring = false
 	emit_signal("attack_finished")
